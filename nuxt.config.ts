e8e3ff72eaf4b20afbf8fd419be1cef2ebffa634
azimaet <maximedolo@gmail.com>
2023-12-28 08:48:19 +0100

    init. add pandacss style system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
+  alias: {
+    "styled-system": resolve("./styled-system"),
+  },
+
+  css: ["@/assets/css/global.css"],
+
+  postcss: {
+    plugins: {
+      "@pandacss/dev/postcss": {},
+    },
+  },
 });
